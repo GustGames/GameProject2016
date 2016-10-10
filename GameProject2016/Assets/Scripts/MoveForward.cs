cc8668454d3bcfc44afe78c92e10cc78f36e8cd3
Plip101 <Bazman15@gmail.com>
2016-10-10 17:16:26 +0100

    Barry Branch
    full first iterration Build

--- GameProject2016/Assets/Scripts/MoveForward.cs
+++ GameProject2016/Assets/Scripts/MoveForward.cs
@@ -3,21 +3,30 @@ using System.Collections;
 
 public class MoveForward : MonoBehaviour {
 
-	public static float speed;
-	public float cruisespeed = 30.0f;
-
-	void Start(){
-		speed = 1;
-
-	}
+	public static float speed = 30.0f;
+	public static float standardSpeed = 50.0f;
+	public  float acceleration = 0.1f;
+	
 	// Update is called once per frame
 	void Update () {
-		if (speed < cruisespeed) {
-			speed  += 0.1f;
+
+		if (speed < standardSpeed) {
+			speed  += acceleration;			//Increasing speed.
 		}
-		transform.position += transform.forward*speed*Time.deltaTime;
+		if (speed > standardSpeed) {
+			speed -= 0.2f;						//If speed is above the standardSpeed, deccelerate until it is not.
+		}
+
+		transform.position += transform.forward*speed*Time.deltaTime;		//Actual movement.
 		if (speed >= 50) {
-			print (speed);
+			print("PRESS SPACE TO WARP");
 		}
 	}
+
+	public static void SpeedRingBoost() 
+	{
+		Debug.Log ("BOOST IN SPEED!");
+		speed += 90.0f;
+	}
+
 }
